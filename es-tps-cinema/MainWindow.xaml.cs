047db98e67141a4f6bb36671e4ae4d16a94de60e
zammatommy <71379782+zammatommy@users.noreply.github.com>
2021-02-20 14:15:51 +0100

    Aggiunta Metodo Svuota cinema e controllo finale

--- es-tps-cinema/MainWindow.xaml.cs
+++ es-tps-cinema/MainWindow.xaml.cs
@@ -21,46 +21,137 @@ namespace es_tps_cinema
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int B = 0;
         Thread t1;
         Thread t2;
-        private bool[] postiCinema = new bool[6];
-        private object x = new object();
+        bool[] postiCinema = new bool[6];
+        object x = new object();
         int posto;
+        Brush colore = Brushes.Red;
+        Brush colore1 = Brushes.Transparent;
 
         public MainWindow()
         {
             InitializeComponent();
-            // Controllo se i posti sono liberi
-            for (int i = 0; i < postiCinema.Length; i++)
-            {
-                postiCinema[i] = false;
-            }
+            SvuotaCinema();
         }
 
-        private void CassaNumero1()
+        public void CassaNumero1()
         {
-
             lock (x)
             {
                 if (postiCinema[posto] == false)
                 {
+
                     postiCinema[posto] = true;
+
+                    if (posto == 0)
+                    {
+
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto1.Background = colore;
+                        }));
+
+                    }
+                    else if (posto == 1)
+                    {
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto2.Background = colore;
+                        }));
+                    }
+                    else if (posto == 2)
+                    {
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto3.Background = colore;
+                        }));
+                    }
+                    else if (posto == 3)
+                    {
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto4.Background = colore;
+                        }));
+                    }
+                    else if (posto == 4)
+                    {
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto5.Background = colore;
+                        }));
+                    }
+                    else if (posto == 5)
+                    {
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto6.Background = colore;
+                        }));
+                    }
                 }
                 else
                 {
                     MessageBox.Show("Il posto è già occupato");
                 }
             }
+
         }
 
-        private void CassaNumero2()
+        public  void CassaNumero2()
         {
 
             lock (x)
             {
                 if (postiCinema[posto] == false)
                 {
+
                     postiCinema[posto] = true;
+
+                    if (posto == 0)
+                    {
+
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto1.Background = colore;
+                        }));
+
+                    }
+                    else if (posto == 1)
+                    {
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto2.Background = colore;
+                        }));
+                    }
+                    else if (posto == 2)
+                    {
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto3.Background = colore;
+                        }));
+                    }
+                    else if (posto == 3)
+                    {
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto4.Background = colore;
+                        }));
+                    }
+                    else if (posto == 4)
+                    {
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto5.Background = colore;
+                        }));
+                    }
+                    else if (posto == 5)
+                    {
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            lblPosto6.Background = colore;
+                        }));
+                    }
                 }
                 else
                 {
@@ -75,12 +166,15 @@ namespace es_tps_cinema
 
             this.Dispatcher.BeginInvoke(new Action(() =>
             {
-                posto = int.Parse(txtPosto.Text) - 1;
+                posto = int.Parse(txtPosto1.Text) - 1;
             }));
 
-            t1 = new Thread(new ThreadStart(CassaNumero1));
+            t1 = new Thread(new ThreadStart(CassaNumero2));
             t1.Start();
         }
+
+
+
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
@@ -95,10 +189,28 @@ namespace es_tps_cinema
             t2.Start();
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
 
+        public void SvuotaCinema()
+        {
+            for (int i = 0; i < postiCinema.Length; i++)
+            {
+                postiCinema[i] = false;
+            }
+
+            lblPosto1.Background = colore1;
+            lblPosto2.Background = colore1;
+            lblPosto3.Background = colore1;
+            lblPosto4.Background = colore1;
+            lblPosto5.Background = colore1;
+            lblPosto6.Background = colore1;
         }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) //Svuota il cinema
+        {
+            SvuotaCinema();
+        }
+
+        
     }
 }
 
